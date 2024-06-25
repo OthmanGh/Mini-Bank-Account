e05cb1198404fe147d10c0524bc6b3470e39bc09
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-06-25 23:34:44 +0300

    Dispatching customer action & displaying components conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import BalanceDisplay from './features/accounts/BalanceDisplay';
 import AccountOperations from './features/accounts/AccountOperations';
 
-import Customer from './features/customers/CreateCustomer';
+import Customer from './features/customers/Customer';
 import CreateCustomer from './features/customers/CreateCustomer';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const customer = useSelector((state) => state.customer.fullName);
   return (
     <div>
       <h1>🏦 The React-Redux Bank ⚛️</h1>
-      <CreateCustomer />
-      <Customer />
-      <AccountOperations />
-      <BalanceDisplay />
+      {!customer ? (
+        <CreateCustomer />
+      ) : (
+        <>
+          <Customer />
+          <AccountOperations />
+          <BalanceDisplay />
+        </>
+      )}
     </div>
   );
 }
