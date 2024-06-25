4ff254b39574f300d1d55d9efe6b5ef6d5c2e5e1
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-06-25 21:21:44 +0300

    practice redux - working with action creators

--- src/store.js
+++ src/store.js
@@ -33,18 +33,59 @@ function reducer(state = initialeState, action) {
 
 const store = createStore(reducer);
 
-store.dispatch({ type: 'account/deposite', payload: 500 });
-store.dispatch({ type: 'account/withdraw', payload: 200 });
-store.dispatch({
-  type: 'account/requestLoan',
-  payload: {
-    amount: 1000,
-    purpose: 'Buy new Laptop',
-  },
-});
+const ACCOUNT_DEPOSITE = 'account/deposite';
+const ACCOUNT_WITHDRAW = 'account/withdraw';
+const ACCOUNT_REQUESTLOAN = 'account/requestLoan';
+const ACCOUNT_PAYLOAN = 'account/payLoan';
 
-store.dispatch({
-  type: 'account/payLoan',
-});
+// store.dispatch({ type: ACCOUNT_DEPOSITE, payload: 500 });
+// store.dispatch({ type: ACCOUNT_WITHDRAW, payload: 200 });
+// store.dispatch({
+//   type: ACCOUNT_REQUESTLOAN,
+//   payload: {
+//     amount: 1000,
+//     purpose: 'Buy new Laptop',
+//   },
+// });
+
+// store.dispatch({
+//   type: ACCOUNT_PAYLOAN,
+// });
+
+// Action Createers
+function deposite(amount) {
+  return {
+    type: ACCOUNT_DEPOSITE,
+    payload: amount,
+  };
+}
+
+function withdraw(amount) {
+  return {
+    type: ACCOUNT_WITHDRAW,
+    payload: amount,
+  };
+}
+
+function requestLoan(amount, purpose) {
+  return {
+    type: ACCOUNT_REQUESTLOAN,
+    payload: {
+      amount,
+      purpose,
+    },
+  };
+}
+
+function payLoan() {
+  return {
+    type: ACCOUNT_PAYLOAN,
+  };
+}
+
+store.dispatch(deposite(500));
+store.dispatch(withdraw(200));
+store.dispatch(requestLoan(6000, 'Buy Car'));
+store.dispatch(payLoan());
 
 console.log(store.getState());
